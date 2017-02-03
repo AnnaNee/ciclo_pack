2ba720c0eedbd0d52218110ae8f6cfa72392a380
Anna Flavia <anna_flavia@intuit.com>
2017-02-03 00:50:39 -0200

    Side menu; NG Cordova plugin

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('cicloPack', ['ionic', 'cicloPack.routes', 'cicloPack.controllers', 'cicloPack.services'])
+angular.module('cicloPack', ['ionic', 'cicloPack.routes', 'cicloPack.controllers', 'cicloPack.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -6,7 +6,7 @@ angular.module('cicloPack', ['ionic', 'cicloPack.routes', 'cicloPack.controllers
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    
+
     if (window.StatusBar) {
       StatusBar.styleDefault();
     }
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,17 @@
 angular.module('cicloPack.controllers', [])
 
-.controller('homeController', function($scope) {});
+.controller('homeController', function($scope, $ionicSideMenuDelegate, $cordovaBarcodeScanner) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  $scope.scanBarcode = function() {
+      $cordovaBarcodeScanner.scan().then(function(imageData) {
+        alert(imageData.text);
+        console.log("Barcode Format -> " + imageData.format);
+        console.log("Cancelled -> " + imageData.cancelled);
+      }, function(error) {
+        console.log("An error happened -> " + error);
+      });
+  };
+});
